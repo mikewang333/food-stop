475016c81575225a41dd4290702b5d83dac1a263
mikewang333 <mikewang.ca@gmail.com>
2018-01-21 12:28:41 -0800

    almost done w basics

--- main_copy.py
+++ main_copy.py
@@ -12,24 +12,42 @@ app = Flask(__name__)
 #client ID = OkyFz7f00cpLklJ7lzfEHg
 
 class Place:
-	def __init__(self, name, lat, lng, rating):
+	def __init__(self, name):
 		self.name = name
-		self.lat = lat
-		self.lng = lng
-		self.rating = rating
+		#self.lat = lat
+		#self.lng = lng
+		self.rating = None
 		self.has_rating = False
 		self.review_num = 0
 		self.image = ""
 		self.url = ""
+
+	"""def __init__(self, name):
+		self.name = name
+		self.lat = None
+		self.lng = None
+		self.rating = None
+		self.has_rating = False
+		self.review_num = 0
+		self.image = ""
+		self.url = """""
+
+
 	
 	def get_name(self):
 		return self.name;
 	
-	def get_latitude(self):
+	"""def get_latitude(self):
 		return self.lat
 	
 	def get_longitude(self):
 		return self.lng
+
+	def set_latitude(self, lat):
+		self.lat = lat
+	
+	def set_longitude(self, lng):
+		self.lng = lng"""
 	
 	def get_rating(self):
 		return self.rating
@@ -56,14 +74,6 @@ class Place:
 	def set_image(self, image):
 		self.image = image
 
-
-	"""def get_distance(self, destination):
-		starting_latitude = radians(float(self.latitude))
-		ending_latitude = radians(float(destination.get_latitude()))
-		starting_longitude = radians(float(self.longitude))
-		ending_longitude = radians(float(destination.get_longitude()))
-		return 6371.01 * acos(sin(starting_latitude) * sin(ending_latitude) + cos(starting_latitude) * cos(ending_latitude) * cos(starting_longitude - ending_longitude))"""
-
 	def get_has_rating(self):
 		return self.has_rating
 
@@ -75,18 +85,16 @@ class Relation:
 		endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
 		api_key = 'AIzaSyC-g2jSdIeWUogOM5Kvs5D1wFAJ5ePDvcs'
 		#Asks the user to input Where they are and where they want to go.
-		origin = start.replace(' ','+')
-		destination = destination.replace(' ','+')
+		origin = 'San Ramon'.replace(' ','+')
+		end = 'San Jose'.replace(' ','+')
 		#Building the URL for the request
-		nav_request = 'origin={}&destination={}&key={}'.format(origin,destination,api_key)
+		nav_request = 'origin={}&destination={}&key={}'.format(origin,end,api_key)
 		request = endpoint + nav_request
 		#Sends the request and reads the response.
 		response = urllib.request.urlopen(request).read()
 		#Loads response as JSON
 		directions = json.loads(response)
 		info = directions['routes'][0]['legs'][0]
-
-
 		self.start_location_lat = info['start_location']['lat']
 		self.start_location_lng = info['start_location']['lng']
 		self.end_location_lat = info['end_location']['lat']
@@ -97,18 +105,18 @@ class Relation:
 		self.distance = info['distance']['value']
 		#all steps
 		self.steps = info['steps']
-
+		print('hello')
 
 
 	def get_distance(self):
-		return self.duration
+		return self.distance
 
 	def get_duration(self):
 		return self.duration
 
 	def approx_location(self, curr_location_lat, curr_location_lng, next_location_lat, next_location_lng, step_duration, timetilleat):
 		#fraction is the portion we completed of the step
-		fraction = (step_duration / timetilleat)
+		fraction = (timetilleat / step_duration)
 		approx_location_lat = ((next_location_lat - curr_location_lat) * fraction) + curr_location_lat 
 		approx_location_lng = ((next_location_lng - curr_location_lng) * fraction) + curr_location_lng
 		return approx_location_lat, approx_location_lng
@@ -132,135 +140,44 @@ class Relation:
 				next_location_lat = self.steps[i]['end_location']['lat']
 				next_location_lng = self.steps[i]['end_location']['lng']
 				if step_duration > timetilleat:
-					one,two = self.approx_location(curr_location_lat, curr_location_lng, next_location_lat, next_location_lng, step_duration, timetilleat)
-					print('hey' + str(one) + 'and' + str(two))
-					return 1,2
+					return self.approx_location(curr_location_lat, curr_location_lng, next_location_lat, next_location_lng, step_duration, timetilleat)
 				#go to next step!
 				else:
 					curr_location_lat = next_location_lat
 					curr_location_lng = next_location_lng
 					timetilleat -= step_duration
 
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-def query_header_constructor(query, at_bool, at_val, in_val):
-	"""Generates a dictionary named headers which stores a common set of parameters for the get requests in search_query, autosuggest_query, and explore_query"""
-	APP_ID = "JlpRxY5ZfJmmqFMIoiGe"
-	APP_CODE = "_0Wcx47HZmzeB48BpMHGGg"
-	headers = dict()
-	headers["app_id"] = APP_ID
-	headers["app_code"] = APP_CODE
-	if query is not None:
-		headers["q"] = query
-	if at_bool:
-		headers["at"] = at_val
-	# else:
-	# 	headers["in"] = in_val
-	return headers
-
-def places_lst_constructor(results, number_of_places):
-	"""Takes in the results of a previous query and returns a list of Places with a size equal to the number of places requested"""
-	places = []
-	for x in range(0, number_of_places):
-		target_place = results[x]
-		rating = None
-		if "averageRating" in target_place:
-			rating = target_place.get("averageRating")
-		places.append(Place(target_place["title"], target_place["position"][0], target_place["position"][1], rating))
-	return places
-
-#fixing route
-def search_query(query, number_of_places, at_bool, at_val, in_val, lat1, lon1, lat2, lon2):
-	"""Does a search query(Nokia Here Places API) and returns a list of places"""
-	url = query_url_constructor("/discover/search")
-	headers = query_header_constructor(query, at_bool, at_val, in_val)
-	headers["route"] = "[" + str(lat1) + "," + str(lon1) + "|" + str(lat2) + "," + str(lon2) + "];w=100000" 
-	r = requests.get(url, params=headers)
-	s = json.loads(r.text)
-	results = s["results"]
-	results = results["items"]
-	return places_lst_constructor(results, number_of_places)
-
-
-place1 = Place("Seattle", 47.608013, -122.335167, None)
-place2 = Place("San Francisco", 37.773972, -122.431297, None)
-place3 = Place("Los Angeles", 34.052235, -118.243683, None)
-
-#Test for four_category_lists
-# lst = search_query("Museum", 3, False, "47.608013,-122.335167", center_circle_constructor(place2, place1), 37.773972, -122.431297, 34.052235, -118.243683)
-# for x in range(0,3):
-# 	print(lst[x].get_name())
-
-# lst = search_query("Museum", 3, False, "47.608013,-122.335167", center_circle_constructor(place2, place1))
-# for x in range(0,3):
-# 	print(lst[x].get_name())
-
-
-def four_category_lists(choices_list, number_of_places, at_bool, at_val, starting_place, destination):
-	for category in choices_list:
-		lst = search_query(category, number_of_places, at_bool, at_val, center_circle_constructor(starting_place, destination))
-		for x in range(0, number_of_places):
-			print(lst[x].get_name())
-
-
-def yelp_api_set_rating_url_review_text(term, latitude, longitude, place):
+#place1 = Place("Seattle", 47.608013, -122.335167, None)
+#place2 = Place("San Francisco", 37.773972, -122.431297, None)
+#place3 = Place("Los Angeles", 34.052235, -118.243683, None)
+def yelp_api_set_rating_url_review_text(timetoeat, lat, lng, n=5):
 	#API Key = GMeB-1KoNJ0XvhlOn9VFe4GPRCIWMusT-B-dwIYVSb0oG22RXFiUoBxz3gNvq4dTX2LenL6HWQwXoyLs9oJyFOIPWGFLRkPpKXo1KfCnENimVObU_vKl1clzLYZNWnYx
 	url = "https://api.yelp.com/v3/businesses/search"
 	headers = {
         'Authorization': 'Bearer GMeB-1KoNJ0XvhlOn9VFe4GPRCIWMusT-B-dwIYVSb0oG22RXFiUoBxz3gNvq4dTX2LenL6HWQwXoyLs9oJyFOIPWGFLRkPpKXo1KfCnENimVObU_vKl1clzLYZNWnYx',
 	}
 	url_params = {
-        'term': term.replace(' ', '+'),
-        'latitude': latitude,
-        'longitude': longitude,
-        'limit': 10
-    }
-	response = requests.request('GET', url, headers=headers, params=url_params)
-	s = json.loads(response.text)
-	for p in s['businesses']:
-		if p['name'] == term:
-			place.set_rating(p['rating'])
-			place.set_url(p['url'])
-			place.set_image(p['image_url'])
-			place.set_review_num(p['review_count'])
-			return
-	place.set_rating(0)
-	place.set_url("")
-	place.set_image("")
-	place.set_review_num(0)
-	return
+        #'term': term.replace(' ', '+'),
+        'categories': 'Restaurants',
+        'latitude': lat,
+        'longitude': lng,
+        'sort_by': 'rating',
+        #'open_at': timetoeat,
+        'limit': n
 
-def yelp_api_set_rating(term, latitude, longitude):
-	#uses yelp api to get a place's rating
-	url = "https://api.yelp.com/v3/businesses/search"
-	headers = {
-        'Authorization': 'Bearer GMeB-1KoNJ0XvhlOn9VFe4GPRCIWMusT-B-dwIYVSb0oG22RXFiUoBxz3gNvq4dTX2LenL6HWQwXoyLs9oJyFOIPWGFLRkPpKXo1KfCnENimVObU_vKl1clzLYZNWnYx',
-	}
-	url_params = {
-        'term': term.replace(' ', '+'),
-        'latitude': latitude,
-        'longitude': longitude,
-        'limit': 10
     }
 	response = requests.request('GET', url, headers=headers, params=url_params)
 	s = json.loads(response.text)
-	for place in s['businesses']:
-		if place['name'] == term:
-			rating = place['rating']
-			return rating
-	return "Place has no rating"
+	restaurant_list = []
+	for r in s['businesses']:
+		new_r = place(r['name'])
+		new_r.set_rating(p['rating'])
+		new_r.set_url(p['url'])
+		new_r.set_image(p['image_url'])
+		new_r.set_review_num(p['review_count'])
+		restaurant_list.append(new_r)
+	return restaurant_list
+
 def star_url(number_of_stars):
 	star_path = "/static/yelp_stars/web_and_ios/large/"
 	if number_of_stars == 0:
@@ -285,13 +202,6 @@ def star_url(number_of_stars):
 		star_path += "large_4_half.png"
 	return star_path
 
-#ranks places to visit based on ratings
-def place_ranker(lst_places):
-	new_lst = sorted(lst_places, key=methodcaller('get_rating'))[::-1]
-	to_return = []
-	for place in new_lst:
-		to_return.append(place.get_name())
-	return to_return
 
 @app.route('/')
 def welcome_to_food_stop():
@@ -303,23 +213,18 @@ def form():
 
 @app.route('/submitted', methods=['POST'])
 def submitted_form():
-    global place_relation
-    start = request.form['place1']
-    destination = request.form['place2']
-    strtime = request.form['timetoeat']
-    timetoeat = datetime.strptime(strtime, '%H:%M').time()
-
-    place_relation = Relation(start, destination)
-    eat_lat, eat_long = place_relation.get_location_at_time(timetoeat)
-
-    place_lst = search_query("Museum", 5, False, "47.608013,-122.335167", "", 37.773972, -122.431297, 34.052235, -118.243683)
-    for place in place_lst:
-    	yelp_api_set_rating_url_review_text(place.get_name(), place.get_latitude(), place.get_longitude(), place)
-    place_ranker(place_lst)
-    return render_template(
+	global place_lst
+	start = request.form['start']
+	destination = request.form['destination']
+	strtime = request.form['timetoeat']
+	timetoeat = datetime.strptime(strtime, '%H:%M').time()
+	place_relation = Relation(start, destination)
+	eat_lat, eat_lng = place_relation.get_location_at_time(timetoeat)
+	place_lst = yelp_api_set_rating_url_review_text(timetoeat, eat_lat, eat_lng)
+	return render_template(
     'submittedform.html',
-    place1=place1,
-    place2=place2,
+    start=start,
+    destination=destination,
     timetoeat=strtime
     )
 @app.route('/mapinterface', methods=['POST'])
@@ -327,7 +232,6 @@ def first_map():
 	# lst = here.search_query(x,x,x,x)
 	#make dict out of list
 	#take first 5
-    global place_lst
     print(place_lst)
     place1 = place_lst[0]
     place2 = place_lst[1]
